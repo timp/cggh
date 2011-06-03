8f35642537520d3744dd2ea399f9dbf91ead8343
l33h4r7 <l33h4r7@5ccac620-ce07-11de-958f-af26545d0b1a>
2011-06-03 17:40:32 +0000

    SVN problems. Renamed files. Developed files view. Introduced labels.

---
+++ networks/MalariaGEN/projects/dataMerger/trunk/src/main/java/org/cggh/tools/dataMerger/functions/data/merges/MergeFunctions.java
@@ -0,0 +1,245 @@
+package org.cggh.tools.dataMerger.functions.data.merges;
+
+import javax.sql.rowset.CachedRowSet;
+
+import org.cggh.tools.dataMerger.data.merges.MergeModel;
+
+import java.sql.SQLException;
+
+
+
+public class MergeFunctions implements java.io.Serializable {
+
+
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 8859380104215152126L;
+	private CachedRowSet joinsAsCachedRowSet = null;
+	private String joinsAsXHTMLTable = null;
+	private String joinsAsDecoratedXHTMLTable = null;
+	private MergeModel mergeModel;
+	
+	public MergeFunctions() {
+		
+		this.setMergeModel(new MergeModel());
+		
+	}
+
+	
+	public void setJoinsAsCachedRowSet (final CachedRowSet cachedRowSet) {
+		
+		this.joinsAsCachedRowSet = cachedRowSet;
+	}
+	public CachedRowSet getJoinsAsCachedRowSet () {
+		
+		return this.joinsAsCachedRowSet;
+	}	
+	public void setJoinsAsXHTMLTable (final String xhtmlTable) {
+		
+		this.joinsAsXHTMLTable = xhtmlTable;
+	}
+	public String getJoinsAsXHTMLTable () {
+		
+		return this.joinsAsXHTMLTable;
+	}	
+	
+	public void setJoinsAsDecoratedXHTMLTable (final String decoratedXHTMLTable) {
+		this.joinsAsDecoratedXHTMLTable = decoratedXHTMLTable;
+	}
+	public String getJoinsAsDecoratedXHTMLTable () {
+		return this.joinsAsDecoratedXHTMLTable;
+	}	
+	
+	public void setMergeModel (final MergeModel mergeModel) {
+		this.mergeModel = mergeModel;
+	}
+	public MergeModel getMergeModel () {
+		return this.mergeModel;
+	}	
+	
+	
+	public void setJoinsAsDecoratedXHTMLTableUsingJoinsAsCachedRowSet () {
+
+		String decoratedXHTMLTable = null;
+
+			try {
+				if (this.getJoinsAsCachedRowSet().next()) {
+
+					decoratedXHTMLTable = "";
+					
+					decoratedXHTMLTable += "<table class=\"joins-table\">";
+
+					 decoratedXHTMLTable += "<thead>";
+					 decoratedXHTMLTable += "<tr>";
+					 
+					 decoratedXHTMLTable += "<th><!-- Above heading for Col.# --></th>";
+					 decoratedXHTMLTable += "<th><!-- Above heading for Key --></th>";
+					 
+					 //TODO: This path needs to be dynamic.
+					 decoratedXHTMLTable += "<th class=\"file-link-container\"><a href=\"/dataMerger/files/uploads/" + this.getMergeModel().getUpload1Model().getId() + "\">" + this.getMergeModel().getUpload1Model().getOriginalFilename() + "</a></th>";
+					 decoratedXHTMLTable += "<th class=\"file-link-container\"><a href=\"/dataMerger/files/uploads/" + this.getMergeModel().getUpload2Model().getId() + "\">" + this.getMergeModel().getUpload2Model().getOriginalFilename() + "</a></th>";
+					 decoratedXHTMLTable += "<th><!-- Above heading for Column name --></th>";
+					 decoratedXHTMLTable += "<th><!-- Above column for Col.# update buttons --></th>";
+					 decoratedXHTMLTable += "<th><!-- Above column for Remove column button --></th>";
+					 
+					 decoratedXHTMLTable += "</tr>";
+					 decoratedXHTMLTable += "<tr>";
+					 
+					 decoratedXHTMLTable += "<th class=\"column_number-heading\">Col. #</th>";
+					 decoratedXHTMLTable += "<th class=\"key-heading\">Key</th>";
+					 
+					 //TODO: Link these problem messages to a view of the duplicate keys
+					 
+					 if (this.getMergeModel().getDatatable1Model().getDuplicateKeysCount() == 1) {
+						 decoratedXHTMLTable += "<th class=\"problem-message-container\">" + this.getMergeModel().getDatatable1Model().getDuplicateKeysCount() + " duplicate key</th>";
+					 }
+					 else if (this.getMergeModel().getDatatable1Model().getDuplicateKeysCount() > 1) {
+						 decoratedXHTMLTable += "<th class=\"problem-message-container\">" + this.getMergeModel().getDatatable1Model().getDuplicateKeysCount() + " duplicate keys</th>";
+					 } else {
+						 decoratedXHTMLTable += "<th></th>";
+					 }
+					 
+					 if (this.getMergeModel().getDatatable2Model().getDuplicateKeysCount() == 1) {
+						 decoratedXHTMLTable += "<th class=\"problem-message-container\">" + this.getMergeModel().getDatatable2Model().getDuplicateKeysCount() + " duplicate key</th>";
+					 }
+					 else if (this.getMergeModel().getDatatable2Model().getDuplicateKeysCount() > 1) {
+						 decoratedXHTMLTable += "<th class=\"problem-message-container\">" + this.getMergeModel().getDatatable2Model().getDuplicateKeysCount() + " duplicate keys</th>";
+					 } else {
+						 decoratedXHTMLTable += "<th></th>";
+					 }
+					 
+					 decoratedXHTMLTable += "<th class=\"column_name-heading\">Column name</th>";
+					 decoratedXHTMLTable += "<th><!-- Above column for Col.# update buttons --></th>";
+					 decoratedXHTMLTable += "<th><!-- Above column for Remove column button --></th>";
+					 
+					 decoratedXHTMLTable += "</tr>";
+					 decoratedXHTMLTable += "</thead>";
+					 
+					 decoratedXHTMLTable += "<tbody>";
+					 
+					//because next() skips the first row.
+					 this.getJoinsAsCachedRowSet().beforeFirst();
+
+						String rowStripeClassName = "even "; 
+						String rowFirstClassName = "first ";
+						String rowLastClassName = ""; 					 
+					 
+					while (this.getJoinsAsCachedRowSet().next()) {
+						 
+						if (rowStripeClassName == "odd ") {
+							rowStripeClassName = "even ";
+						} else {
+							rowStripeClassName = "odd ";
+						}
+						
+						//TODO: This might need changing when paging.
+						if (this.getJoinsAsCachedRowSet().isLast()) {
+							rowLastClassName = "last ";
+						}
+						
+						decoratedXHTMLTable += "<tr class=\"" + rowStripeClassName + rowFirstClassName + rowLastClassName + "\">";
+
+						decoratedXHTMLTable += "<td class=\"column_number-container\"><input readonly=\"readonly\" type=\"text\" name=\"column_number\" value=\"" + this.getJoinsAsCachedRowSet().getInt("column_number") + "\"/></td>";
+						
+						
+						if (this.getJoinsAsCachedRowSet().getString("datatable_1_column_name") == null || this.getJoinsAsCachedRowSet().getString("datatable_2_column_name") == null) {
+							//Can only have a key relevant to both tables.
+							decoratedXHTMLTable += "<td class=\"key-container\"><input type=\"checkbox\" name=\"key-" + this.getJoinsAsCachedRowSet().getInt("column_number") + "\" value=\"" + this.getJoinsAsCachedRowSet().getBoolean("key") + "\" disabled=\"disabled\" /></td>";							
+						} else {
+							if (this.getJoinsAsCachedRowSet().getBoolean("key")) {
+								decoratedXHTMLTable += "<td class=\"key-container\"><input type=\"checkbox\" name=\"key-" + this.getJoinsAsCachedRowSet().getInt("column_number") + "\" value=\"" + this.getJoinsAsCachedRowSet().getBoolean("key") + "\" checked=\"checked\"/></td>";
+							} else {
+								decoratedXHTMLTable += "<td class=\"key-container\"><input type=\"checkbox\" name=\"key-" + this.getJoinsAsCachedRowSet().getInt("column_number") + "\" value=\"" + this.getJoinsAsCachedRowSet().getBoolean("key") + "\" /></td>";
+							}
+						}
+							
+						if (this.getJoinsAsCachedRowSet().getString("datatable_1_column_name") != null) {
+							decoratedXHTMLTable += "<td class=\"datatable_1_column_name-container\">";
+							decoratedXHTMLTable += "<input type=\"text\" readonly=\"readonly\" name=\"datatable_1_column_name-" + this.getJoinsAsCachedRowSet().getInt("column_number") + "\" value=\"" + this.getJoinsAsCachedRowSet().getString("datatable_1_column_name") + "\"/>";
+							
+							//TODO
+							//decoratedXHTMLTable += "<textarea readonly=\"readonly\">TODO: Sample of data</textarea>";
+							
+							decoratedXHTMLTable += "</td>";
+						} else {
+							if (this.getJoinsAsCachedRowSet().getString("constant_1") != null) {
+								decoratedXHTMLTable += "<td class=\"constant_1-container\"><label for=\"constant_1-" + this.getJoinsAsCachedRowSet().getInt("column_number") + "\">Constant:</label><input type=\"text\" readonly=\"readonly\" name=\"constant_1-" + this.getJoinsAsCachedRowSet().getInt("column_number") + "\" value=\"" + this.getJoinsAsCachedRowSet().getString("constant_1") + "\"/></td>";
+							} else {
+								decoratedXHTMLTable += "<td class=\"null-container\">NULL</td>";
+							}
+						}
+							
+						
+						
+						if (this.getJoinsAsCachedRowSet().getString("datatable_2_column_name") != null) {
+							decoratedXHTMLTable += "<td class=\"datatable_2_column_name-container\">";
+							decoratedXHTMLTable += "<input type=\"text\" readonly=\"readonly\" name=\"datatable_2_column_name-" + this.getJoinsAsCachedRowSet().getInt("column_number") + "\" value=\"" + this.getJoinsAsCachedRowSet().getString("datatable_2_column_name") + "\"/>";
+							
+							//TODO
+							//decoratedXHTMLTable += "<textarea readonly=\"readonly\">TODO: Sample of data</textarea>";
+							
+							decoratedXHTMLTable += "</td>";
+						} else {
+							if (this.getJoinsAsCachedRowSet().getString("constant_2") != null) {
+								decoratedXHTMLTable += "<td class=\"constant_2-container\"><label for=\"constant_2-" + this.getJoinsAsCachedRowSet().getInt("column_number") + "\">Constant:</label><input type=\"text\" readonly=\"readonly\" name=\"constant_2-" + this.getJoinsAsCachedRowSet().getInt("column_number") + "\" value=\"" + this.getJoinsAsCachedRowSet().getString("constant_2") + "\"/></td>";
+							} else {
+								decoratedXHTMLTable += "<td class=\"null-container\">NULL</td>";
+							}
+						}
+						
+						
+						
+						decoratedXHTMLTable += "<td class=\"column_name-container\"><input type=\"text\" name=\"column_name\" value=\"" + this.getJoinsAsCachedRowSet().getString("column_name") + "\" /></td>";
+						
+						//TODO: Improve move function calls/naming.
+						
+						decoratedXHTMLTable += "<td class=\"move-row-buttons-container\">";
+						
+						if (this.getJoinsAsCachedRowSet().getInt("column_number") > 1) {
+							decoratedXHTMLTable += "<button class=\"move up\"><img src=\"/dataMerger/pages/shared/png/up.png\" title=\"Up\" /></button>";
+						} else {
+							decoratedXHTMLTable += "<button class=\"move up\" disabled=\"disabled\"><img src=\"/dataMerger/pages/shared/png/up.png\" title=\"Up\" /></button>";
+						}
+						
+						if (this.getJoinsAsCachedRowSet().getInt("column_number") < this.getJoinsAsCachedRowSet().size()) {
+							decoratedXHTMLTable += "<button class=\"move down\"><img src=\"/dataMerger/pages/shared/png/down.png\" title=\"Down\" /></button>";
+						} else {	
+							decoratedXHTMLTable += "<button class=\"move down\" disabled=\"disabled\"><img src=\"/dataMerger/pages/shared/png/down.png\" title=\"Down\" /></button>";
+						}
+						
+						decoratedXHTMLTable += "</td>";
+						
+						decoratedXHTMLTable += "<td class=\"remove-button-container\"><button class=\"remove\">Remove</button></td>";
+						
+						decoratedXHTMLTable += "</tr>";
+						
+						rowFirstClassName = "";
+					  }
+
+					decoratedXHTMLTable += "</tbody>";
+					 
+					decoratedXHTMLTable += "</table>";
+					
+					decoratedXHTMLTable += "<!-- <div>TODO: paging</div> -->";
+					
+	
+				} else {
+					
+					decoratedXHTMLTable = "There are no columns.";
+					
+				}
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+		
+		this.setJoinsAsDecoratedXHTMLTable(decoratedXHTMLTable);
+	
+	}
+
+
+	
+}
---
+++ networks/MalariaGEN/projects/dataMerger/trunk/src/main/java/org/cggh/tools/dataMerger/functions/data/merges/MergesFunctions.java
@@ -0,0 +1,172 @@
+package org.cggh.tools.dataMerger.functions.data.merges;
+
+import java.sql.SQLException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+
+import javax.sql.rowset.CachedRowSet;
+
+
+public class MergesFunctions implements java.io.Serializable {
+
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1021181895735718787L;
+	private CachedRowSet mergesAsCachedRowSet;
+	private String mergesAsDecoratedXHTMLTable;
+
+	public MergesFunctions() {
+		
+		
+	}
+
+	public void setMergesAsCachedRowSet(CachedRowSet mergesAsCachedRowSet) {
+		this.mergesAsCachedRowSet = mergesAsCachedRowSet;
+	}
+
+	public void setMergesAsDecoratedXHTMLTableUsingMergesAsCachedRowSet() {
+		
+		String mergesAsDecoratedXHTMLTable = null;
+		
+		try {
+			if (this.getMergesAsCachedRowSet().next()) {
+
+				mergesAsDecoratedXHTMLTable = "";
+				
+				mergesAsDecoratedXHTMLTable = mergesAsDecoratedXHTMLTable.concat("<table class=\"merges-table\">");
+
+				 mergesAsDecoratedXHTMLTable = mergesAsDecoratedXHTMLTable.concat("<thead>");
+				 mergesAsDecoratedXHTMLTable = mergesAsDecoratedXHTMLTable.concat("<tr>");
+				 
+				 mergesAsDecoratedXHTMLTable = mergesAsDecoratedXHTMLTable.concat("<th>ID<!--  <a href=\"javascript:TODOsort();\">sort up/down</a> --></th>");
+				 mergesAsDecoratedXHTMLTable = mergesAsDecoratedXHTMLTable.concat("<th>File 1<!--  <a href=\"javascript:TODOsort();\">sort up/down</a> --></th>");
+				 mergesAsDecoratedXHTMLTable = mergesAsDecoratedXHTMLTable.concat("<th>File 2<!--  <a href=\"javascript:TODOsort();\">sort up/down</a> --></th>");
+				 mergesAsDecoratedXHTMLTable = mergesAsDecoratedXHTMLTable.concat("<th>Created<!--  <a href=\"javascript:TODOsort();\">sort up/down</a> --></th>");
+				 mergesAsDecoratedXHTMLTable = mergesAsDecoratedXHTMLTable.concat("<th>Updated<!--  <a href=\"javascript:TODOsort();\">sort up/down</a> --></th>");
+				 mergesAsDecoratedXHTMLTable = mergesAsDecoratedXHTMLTable.concat("<th><!-- Column for edit-join links --></th>");
+				 mergesAsDecoratedXHTMLTable = mergesAsDecoratedXHTMLTable.concat("<th><!-- Column for edit-resolutions links or duplicate keys count--></th>");
+				 mergesAsDecoratedXHTMLTable = mergesAsDecoratedXHTMLTable.concat("<th><!-- Column for export button or conflicts count --></th>");
+				 
+				 mergesAsDecoratedXHTMLTable = mergesAsDecoratedXHTMLTable.concat("</tr>");
+				 mergesAsDecoratedXHTMLTable = mergesAsDecoratedXHTMLTable.concat("</thead>");
+				 
+				 mergesAsDecoratedXHTMLTable = mergesAsDecoratedXHTMLTable.concat("<tbody>");
+				 
+				//because next() skips the first row.
+				 this.getMergesAsCachedRowSet().beforeFirst();
+
+					String rowStripeClassName = "even "; 
+					String rowFirstClassName = "first ";
+					String rowLastClassName = ""; 
+					
+					DateFormat dateFormat = new SimpleDateFormat("dd MMM yyyy HH:mm");  
+				 
+				while (this.getMergesAsCachedRowSet().next()) {
+					 
+					if (rowStripeClassName == "odd ") {
+						rowStripeClassName = "even ";
+					} else {
+						rowStripeClassName = "odd ";
+					}
+					
+					//TODO: This might need changing when paging.
+					if (this.getMergesAsCachedRowSet().isLast()) {
+						rowLastClassName = "last ";
+					}
+					
+					
+					mergesAsDecoratedXHTMLTable = mergesAsDecoratedXHTMLTable.concat("<tr class=\"" + rowStripeClassName + rowFirstClassName + rowLastClassName + "\">");
+
+					 mergesAsDecoratedXHTMLTable = mergesAsDecoratedXHTMLTable.concat("<td><input type=\"hidden\" name=\"merge_id\" value=\"" + this.getMergesAsCachedRowSet().getString("id")  + "\"/>" + this.getMergesAsCachedRowSet().getString("id") + "</td>");
+					 
+					 //FIXME: Apparently a bug in CachedRowSet using getX('columnAlias') aka columnLabel, which actually only works with getX('columnName'), so using getX('columnIndex').
+					 
+					 //TODO: This URL shouldn't be hard-coded
+					 mergesAsDecoratedXHTMLTable = mergesAsDecoratedXHTMLTable.concat("<td><a href=\"/dataMerger/files/uploads/" + this.getMergesAsCachedRowSet().getInt(2) + "\">" + this.getMergesAsCachedRowSet().getString(3) + "</a></td>");
+					 mergesAsDecoratedXHTMLTable = mergesAsDecoratedXHTMLTable.concat("<td><a href=\"/dataMerger/files/uploads/" + this.getMergesAsCachedRowSet().getInt(4) + "\">" + this.getMergesAsCachedRowSet().getString(5) + "</a></td>");
+					 //TODO: format datetime 02 Jan 2011
+					 mergesAsDecoratedXHTMLTable = mergesAsDecoratedXHTMLTable.concat("<td>" + dateFormat.format(this.getMergesAsCachedRowSet().getTimestamp("created_datetime")) + "</td>");
+					 mergesAsDecoratedXHTMLTable = mergesAsDecoratedXHTMLTable.concat("<td>" + dateFormat.format(this.getMergesAsCachedRowSet().getTimestamp("updated_datetime")) + "</td>");
+					 
+					 //TODO: Change this URL to a) not hard-coded b) /dataMerger/pages/merges/3/joins
+					 mergesAsDecoratedXHTMLTable = mergesAsDecoratedXHTMLTable.concat("<td><a href=\"/dataMerger/pages/merges/joins?merge_id=" + this.getMergesAsCachedRowSet().getInt("id") + "\">Edit Join</a></td>");
+					 
+					 //FIXME: totalDuplicateKeysCount == 0, even when null in db
+					 
+					 Integer totalDuplicateKeysCount = this.getMergesAsCachedRowSet().getInt("total_duplicate_keys_count");
+					 
+					 if (totalDuplicateKeysCount != null) {
+						 
+						 if (totalDuplicateKeysCount == 0) {
+							 //TODO: Change this URL to a) not hard-coded b) /dataMerger/pages/merges/3/resolutions-by-column
+							 mergesAsDecoratedXHTMLTable = mergesAsDecoratedXHTMLTable.concat("<td><a href=\"/dataMerger/pages/merges/resolutions/by-column?merge_id=" + this.getMergesAsCachedRowSet().getInt("id") + "\">Edit Resolutions</a></td>");
+						 } 
+						 else if (totalDuplicateKeysCount == 1) {
+							 mergesAsDecoratedXHTMLTable = mergesAsDecoratedXHTMLTable.concat("<td class=\"problem-message-container\">" + totalDuplicateKeysCount + " duplicate key</td>");
+						 }
+						 else {
+							 mergesAsDecoratedXHTMLTable = mergesAsDecoratedXHTMLTable.concat("<td class=\"problem-message-container\">" + totalDuplicateKeysCount + " duplicate keys</td>");
+						 }
+						 
+					 } else {
+						 mergesAsDecoratedXHTMLTable = mergesAsDecoratedXHTMLTable.concat("<td><!-- pending a total duplicate keys count --></td>");
+					 }
+					
+					 //FIXME: totalConflictsCount == 0, even when null in db
+					 
+					 Integer totalConflictsCount = this.getMergesAsCachedRowSet().getInt("total_conflicts_count");
+					 
+					 if (totalConflictsCount != null && totalDuplicateKeysCount == 0) {
+						 
+						 if (totalConflictsCount == 0) {
+							 //TODO: Wire this up to AJAX (or direct JS call)
+							 mergesAsDecoratedXHTMLTable = mergesAsDecoratedXHTMLTable.concat("<td><button class=\"export-button\">Export</button><img class=\"exporting-indicator\" src=\"/dataMerger/pages/shared/gif/loading.gif\" style=\"display:none\" title=\"Exporting...\"/></td>");
+						 }
+						 else if (totalConflictsCount == 1) {
+							 mergesAsDecoratedXHTMLTable = mergesAsDecoratedXHTMLTable.concat("<td class=\"problem-message-container\">" + totalConflictsCount + " conflict</td>");
+						 } else {
+							 mergesAsDecoratedXHTMLTable = mergesAsDecoratedXHTMLTable.concat("<td class=\"problem-message-container\">" + totalConflictsCount + " conflicts</td>");
+						 } 
+					 
+					 } else {
+						 mergesAsDecoratedXHTMLTable = mergesAsDecoratedXHTMLTable.concat("<td><!-- pending a total conflicts count --></td>");
+					 }
+					 
+					 mergesAsDecoratedXHTMLTable = mergesAsDecoratedXHTMLTable.concat("</tr>");
+					 
+					 rowFirstClassName = "";
+				  }
+
+				mergesAsDecoratedXHTMLTable = mergesAsDecoratedXHTMLTable.concat("</tbody>");
+				 
+				mergesAsDecoratedXHTMLTable = mergesAsDecoratedXHTMLTable.concat("</table>");
+				
+				mergesAsDecoratedXHTMLTable = mergesAsDecoratedXHTMLTable.concat("<!-- <div>TODO: paging</div> -->");
+				
+			} else {
+				
+				mergesAsDecoratedXHTMLTable = "<p>You have no merges.</p>";
+				
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		this.mergesAsDecoratedXHTMLTable = mergesAsDecoratedXHTMLTable;
+	}
+
+	public CachedRowSet getMergesAsCachedRowSet() {
+		return this.mergesAsCachedRowSet;
+	}
+
+	public String getMergesAsDecoratedXHTMLTable() {
+		return this.mergesAsDecoratedXHTMLTable;
+	}
+
+	
+
+	
+}
