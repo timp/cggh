c39153add811b44927d1660e56cd544417bf66b0
l33h4r7 <l33h4r7@5ccac620-ce07-11de-958f-af26545d0b1a>
2011-06-22 10:41:40 +0000

    Vanilla vanilla.

---
+++ networks/MalariaGEN/projects/Vanilla/trunk/src/main/java/org/cggh/tools/vanilla/data/DataController.java
@@ -0,0 +1,39 @@
+package org.cggh.tools.vanilla.data;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class DataController
+ */
+public class DataController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public DataController() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		response.setContentType("text/plain");
+		response.getWriter().println("Unhandled Header Accept: " + request.getHeader("Accept"));
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
